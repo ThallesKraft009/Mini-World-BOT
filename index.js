3664dd27019938af399c8c1ea655c4fa0606d1cd
ThallesKraft009 <99352499+ThallesKraft009@users.noreply.github.com>
2024-02-15 00:06:15 +0000

    Anti erro add

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ new MiniWorldBOT().start()
 process.on('uncaughtException', (err) => {
   console.error('Erro não capturado:', err);
 });
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Rejeição não tratada:', reason);
+});
